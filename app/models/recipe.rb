d7338501a082bab3e92ed4c9a2d697d053229ed8
Jason Shen <sikajs@gmail.com>
2016-07-03 11:14:46 +0800

    Fixed issue for unable to save directions

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -15,7 +15,7 @@
 #
 
 class Recipe < ActiveRecord::Base
-
+	belongs_to :user
 	has_many :ingredients
 	has_many :directions
 
@@ -23,7 +23,7 @@ class Recipe < ActiveRecord::Base
 																reject_if: proc { |attributes| attributes['name'].blank? },
 																allow_destroy: true
 	accepts_nested_attributes_for :directions,
-																reject_if: proc { |attributes| attributes['name'].blank? },
+																reject_if: proc { |attributes| attributes['step'].blank? },
 																allow_destroy: true													
 
 	validates :title, :description, :image, presence: true
